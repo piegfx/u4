383351f1fa54ba71702997e7328a85c0dca437ed
ollie <ollie@ollierobinson.co.uk>
2024-02-15 12:46:49 +0000

    size struct

--- src/u4.Math/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace u4.Math;
-
-public class Class1
-{
-
-}
---
+++ src/u4.Math/Size.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace u4.Math;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct Size<T> : IEquatable<Size<T>> where T : INumber<T>
+{
+    public T Width;
+
+    public T Height;
+
+    public Size(T wh)
+    {
+        Width = wh;
+        Height = wh;
+    }
+
+    public Size(T width, T height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Size<TOther> As<TOther>() where TOther : INumber<TOther>
+        => new Size<TOther>(TOther.CreateChecked(Width), TOther.CreateChecked(Height));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static explicit operator System.Drawing.Size(in Size<T> size)
+        => new System.Drawing.Size(int.CreateChecked(size.Width), int.CreateChecked(size.Height));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static explicit operator Size<T>(in System.Drawing.Size size)
+        => new Size<T>(T.CreateChecked(size.Width), T.CreateChecked(size.Height));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Size<T> operator +(in Size<T> left, in Size<T> right)
+        => new Size<T>(left.Width + right.Width, left.Height + right.Height);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Size<T> operator -(in Size<T> left, in Size<T> right)
+        => new Size<T>(left.Width - right.Width, left.Height - right.Height);
+
+
+    public static bool operator ==(Size<T> left, Size<T> right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Size<T> left, Size<T> right)
+    {
+        return !left.Equals(right);
+    }
+    
+    public bool Equals(Size<T> other)
+    {
+        return Width == other.Width && Height == other.Height;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Size<T> other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Width, Height);
+    }
+
+    public override string ToString()
+    {
+        return $"(Width: {Width}, Height: {Height})";
+    }
+}
