4fb4bef5406753446beddd42d47abd78a86c8295
ollie <ollie@ollierobinson.co.uk>
2024-03-05 23:39:14 +0000

    started input layouts

---
+++ src/u4.Render.Backend/Format.cs
@@ -0,0 +1,21 @@
+﻿namespace u4.Render.Backend;
+
+/// <summary>
+/// Represents supported data formats. Used in textures and input layouts, among other things. Based on DXGI_FORMAT, but
+/// not cast-compatible.
+/// </summary>
+public enum Format
+{
+    R8UNorm,
+    R8UInt,
+    R8SNorm,
+    R8SInt,
+    
+    R16UNorm,
+    R16UInt,
+    R16SNorm,
+    R16SInt,
+    R16Float,
+    
+    
+}
---
+++ src/u4.Render.Backend/InputLayoutDescription.cs
@@ -0,0 +1,8 @@
+﻿namespace u4.Render.Backend;
+
+public struct InputLayoutDescription
+{
+    public string SemanticName;
+    public uint SemanticIndex;
+    public Format Format;
+}
